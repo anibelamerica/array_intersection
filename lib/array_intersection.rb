33b94ef97a2d5598869a4909772be4e003d37726
Anibel America <ameriabel@gmail.com>
2018-10-07 11:04:29 -0700

    adds iterative implementation of array_intersection algorithm

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,18 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+
+  intersection = []
+
+  unless array1 == nil || array2 == nil || array1.empty? || array2.empty?
+    array1.each do |i|
+      array2.each do |j|
+        if i == j
+          intersection << i
+        end
+      end
+    end
+  end
+
+  return intersection
+
 end
